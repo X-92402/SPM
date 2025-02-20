d37764dff79de3194c1c7ababb379fd4b57804ed
tianyinnn_lim <tianyinnnlim@gmail.com>
2025-02-20 20:06:56 +0800

    add favicon

--- header.php
+++ header.php
@@ -10,6 +10,6 @@ require 'database.php';
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <!-- PANGGIL CSS EXTERNAL -->
-<link rel="icon" type="image/png" href="imej/favicon.ico"/>
+<link rel="icon" type="image/png" href="gambar/favicon.ico"/>
 <link rel="stylesheet" type="text/css" href="style.css">
 <title><?php echo $namasys;?></title>
